1057819707fd8df96da77f830beb49ccc4643ff1
Thomas Fel <tfel@g.harvard.edu>
2025-02-09 02:21:36 -0500

    Bump version: 0.1.0 → 0.2.0

--- overcomplete/__init__.py
+++ overcomplete/__init__.py
@@ -2,7 +2,7 @@
 Overcomplete: Personal toolbox for experimenting with Dictionary Learning.
 """
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 
 from .optimization import (SkPCA, SkICA, SkNMF, SkKMeans,
